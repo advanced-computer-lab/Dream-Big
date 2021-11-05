fbab3a8684af592043d461bd121157ed9c685a3c
OmarGalal <oelhanafy12@gmail.com>
2021-11-05 17:33:14 +0200

    Implemented Search and List All

---
+++ BackEnd/models/Flights.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+
+const flightSchema = new Schema({
+  From: {
+    type: String,
+    required: true,
+  },
+  To: {
+    type: String,
+    required: true    
+  },
+  'Flight Date': {
+    type: String,
+    required: true,
+  },
+  Cabin: {
+    type: String,
+    required: true
+  },
+  SeatsAvailable: {
+    type: Number,
+    required: true
+  }
+}, { timestamps: true });
+
+const Flight = mongoose.model('Flight', flightSchema);
+module.exports = Flight;
