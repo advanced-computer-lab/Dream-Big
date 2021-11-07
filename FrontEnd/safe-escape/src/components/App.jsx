8ea8323ac7d3abc5a86a5e67769a4f8cd53819be
SarahHany <sarahhany225@gmail.com>
2021-11-07 19:52:13 +0200

    Sarah

---
+++ FrontEnd/safe-escape/src/components/App.jsx
@@ -0,0 +1,14 @@
+//import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import NavBar from "./NavBar";
+import Delete from './Delete';
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <Delete />
+  )
+}
+
+export default App;
