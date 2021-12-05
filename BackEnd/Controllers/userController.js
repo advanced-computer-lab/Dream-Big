5ae893e470c219f877f30e8d722b0ec9b44ce962
Yousef Kamal <yousef.hassanein@student.guc.edu.eg>
2021-12-05 17:57:42 +0200

    no omar

--- BackEnd/Controllers/userController.js
+++ BackEnd/Controllers/userController.js
@@ -1,60 +1,6 @@
 const router = require('express').Router();
-let Flights = require('../models/Flights');
 let Users = require('../models/Users');
 
-router.post('/getUserSearch', async (req, res) => {
-    //const flight =new Flights( req.body);
-    try {
-        //await flight.save();
-        const flights = await Flights.find(req.body);
-
-        console.log("FLIGHTSSS", flights);
-        res.send(flights);
-
-    }
-    catch (err) {
-        console.log("ERRORR");
-        res.status(500).send(err)
-    }
-});
-
-router.put('/:id', async (req, res, next) => {
-    try {
-        const toUpdate = req.body.toBeUpdatedInUser;
-        const result = await Users.findByIdAndUpdate(req.params.id, toUpdate, { new: true });
-        await Users.save;
-        console.log(result)
-    }
-    catch (err) {
-        console.log("ERRORR");
-        res.status(500).send(err)
-    }
-});
-
-router.get('/UserDetails/:id', async (req, res) => {
-    console.log('id', req.params.id)
-    try {
-        const id = req.params.id;
-        const users = await Users.findById(id).populate('ReservedFlights')
-        console.log(users);
-        res.send(users);
-    }
-    catch (err) {
-        res.status(500).send(err)
-    }
-});
-
-router.put('/:id', async (req, res, next) => {
-    try {
-        const toUpdate = req.body.updateReservedFlights;
-        const result = await Users.findByIdAndUpdate(req.params.id, { ReservedFlights: toUpdate }, { new: true });
-        await Users.save;
-        res.send(result);
-    }
-    catch (err) {
-        console.log(err.message);
-    }
-});
 
 router.route('/getUsers').get(async (req, res) => {
     const Users = await Users.find({})
@@ -94,5 +40,6 @@ router.get('/:id/getReservedFlights', async (req, res, next) => {
         console.log(err.message);
     }
 });
+
 
 module.exports = router;
